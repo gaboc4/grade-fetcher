2471cb34596d733cf1c4b363cf3e722fbf25d658
trentduffy <ct_trentduffy@me.com>
2016-04-02 19:30:05 -0400

    arrays for dayz

--- sample.js
+++ sample.js
@@ -14,26 +14,17 @@ casper.start('http://blackboard.neu.edu', function() {
         }, true);
 });
 
-casper.thenOpen('http://blackboard.neu.edu/webapps/gradebook/do/student/viewCourses?returnUrl=/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_3_1&tabId=_1_1&forwardUrl=index.jsp', function() {
-//    this.captureSelector('capture.png', '.fl-theme-iphone');
-    this.echo("home page hit");
-
-});
 
 
 casper.thenOpen('http://blackboard.neu.edu/webapps/streamViewer/streamViewer?cmd=view&streamName=mygrades_d', function() {
     this.echo("hit grade page");
-    grades = this.evaluate(getGrades);
-
+    grades = document.querySelectorAll(".grade-value");
+    this.echo(grades.toString());
 });
 
 function getGrades() {
     this.echo("get grades function call");
-    var grades = document.querySelectorAll(".grade-value");
-     return Array.prototype.map.call(grades, function(e) {
-         this.echo("grade data" + e.getInnerHTML());
-         return e.getInnerHTML();
-     });
+
 
 }
 
