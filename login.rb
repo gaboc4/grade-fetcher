3b4ee577a5a510fea77e4edae61b76f5f21cfb7b
Gabriel <gabrielc007@gmail.com>
2016-04-03 02:10:41 -0400

    added ruby login but get sstuck at shibboleth

---
+++ login.rb
@@ -0,0 +1,16 @@
+
+# This example logs a user in to rubyforge and prints out the body of the
+# page after logging the user in.
+require 'rubygems'
+require 'mechanize'
+agent = Mechanize.new
+
+page = agent.get('https://neuidmsso.neu.edu/cas-server/login?service=https%3A%2F%2Fneuidmsso.neu.edu%2Fidp%2FAuthn%2FCas%2FNoForceAuthn')
+
+form = page.forms.first
+
+form['username'] = 'centeno.g'
+form['password'] = 'Foohy623'
+page = form.submit
+
+puts page.body
