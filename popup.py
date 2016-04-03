bc61f86974266b2fde3c05494c35ff5414ade5c5
trentduffy <ct_trentduffy@me.com>
2016-04-02 22:14:37 -0400

    this one doens't work lolol

--- popup.py
+++ popup.py
@@ -7,7 +7,7 @@ import time
 timer = 60
 
 # Open the browser
-browser = RoboBrowser(parser='lxml')
+browser = RoboBrowser(user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64)',parser='lxml')
 
 # Open Facebook login page
 browser.open('https://neuidmsso.neu.edu/cas-server/login?service=https%3A%2F%2Fneuidmsso.neu.edu%2Fidp%2FAuthn%2FCas%2FNoForceAuthn')
@@ -15,25 +15,21 @@ browser.open('https://neuidmsso.neu.edu/cas-server/login?service=https%3A%2F%2Fn
 # Grab the login form
 form = browser.get_forms()[0]
 
+print(browser.get_forms()[0])
+
 # Get email and password from user
-form['username'].value = str(input("Email: "))
+form['username'].value = str(raw_input("Username:"))
 form['password'].value = str(getpass.getpass())
+
+# form['username'].value = str("duffy.tr")
+# form['password'].value = str("higabe")
+
+
 
 # Submit login form
 browser.submit_form(form)
 
-# Poke loop
-while True:
-    browser.open('https://m.facebook.com/pokes')
-    pokes = browser.get_links(text="Poke Back")
-    print("%d people Poked you!" % len(pokes))
+# Open Facebook login page
+#browser.open('https://blackboard.neu.edu/webapps/streamViewer/streamViewer?cmd=view&streamName=mygrades_d')
 
-    if len(pokes) > 0:
-        for p in pokes:
-            print("Poked!")
-            browser.follow_link(p)
-        if timer > 2:
-            timer = int(timer / 2)
-    if timer < 61:
-        timer = int(timer * 1.25)
-    time.sleep(timer)
+print(browser.parsed)
